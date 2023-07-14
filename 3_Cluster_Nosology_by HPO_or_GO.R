71ae5928052d1734988bc89ee2bca2c62d89628a
Andreas Zankl <azankl@macbookproaz.wireless.staff.clinical.sydney.edu.au>
2023-07-14 15:58:56 +1000

    started on script 3

---
+++ 3_Cluster_Nosology_by HPO_or_GO.R
@@ -0,0 +1,7 @@
+#Notes on how to cluster nosology by HPO or Go terms
+#based on this paper: https://pubmed.ncbi.nlm.nih.gov/22573485/
+
+#1 get list of all OMIM listed disorders in Nosology
+#2 get list of all HPO terms for each disease (filter HPO annotations list for Nosology disorders)
+#3 or use GO_terms, already built in, might be easier
+#4 look at network packages for the clustering, need to colour the disorders based on their Nosology group
