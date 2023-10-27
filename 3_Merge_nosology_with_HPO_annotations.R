b751dea857518b906f8cfab55ca6828a0f335e94
Andreas Zankl <andreas.zankl@gmail.com>
2023-10-27 15:33:51 +1100

    remove Orpha and Decipher entries from HPOA

--- 3_Merge_nosology_with_HPO_annotations.R
+++ 3_Merge_nosology_with_HPO_annotations.R
@@ -11,12 +11,15 @@ nosology <- read_rds(here("data/nosology_cleaned.rds"))
 #read HPO annotation file (originally downloaded from https://hpo.jax.org/app/data/annotations)
 hpoa <- read_tsv(here("raw_data/HPOA_2023-10-09.tsv"), skip = 4, col_names = TRUE)
 
+#remove ORPHA and DECIPHER entries
+hpoa_OMIM <- filter(hpoa, str_detect(database_id, "OMIM.*"))
+  
 #remove 'OMIM' prefix
-hpoa <- hpoa %>%
+hpoa_OMIM <- hpoa_OMIM %>%
   mutate(database_id = str_replace(database_id,"OMIM:",""))
 
 #keep only HPO annotations for diseases that have an OMIM match in the nosology
-skeldys_hpoa <- filter(hpoa, database_id %in% nosology$NOS_OMIM, )
+skeldys_hpoa <- filter(hpoa_OMIM, database_id %in% nosology$NOS_OMIM, )
 
 hpo_count <- skeldys_hpoa %>%
   add_count(database_id, sort = TRUE)
